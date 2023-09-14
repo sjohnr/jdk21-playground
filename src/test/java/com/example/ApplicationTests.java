3e68229ec072826eec9b0fa3919c8901fddfdb51
Steve Riesenberg <sriesenberg@vmware.com>
2023-09-14 14:29:16 -0500

    Add java toolchain support

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,14 +1,26 @@
 package com.example;
 
+import java.util.concurrent.ThreadFactory;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.task.VirtualThreadTaskExecutor;
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@DisabledOnJre(JRE.JAVA_17)
+	void virtualThreadsAreAvailable() {
+		ThreadFactory threadFactory = new VirtualThreadTaskExecutor().getVirtualThreadFactory();
+		threadFactory.newThread(() -> System.out.println("Hi, I'm on a virtual thread! " + Thread.currentThread().getName()));
 	}
 
 }
